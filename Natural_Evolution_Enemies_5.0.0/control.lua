52ce8038d69cdcec800b5e8a0d40c71723ad888e
TheSAguy <vvwmisc@hotmail.com>
2015-10-08 15:57:39 -0700

    10.8
    Artifact collectors fixed!!

--- Natural_Evolution_Enemies_5.0.0/control.lua
+++ Natural_Evolution_Enemies_5.0.0/control.lua
@@ -34,10 +34,6 @@ local filters = {["small-alien-artifact"] = 1,
 				 }
 
 
-
-
-
-
 game.on_event(defines.events.on_robot_built_entity, function(event) On_Built(event) end)
 game.on_event(defines.events.on_built_entity, function(event) On_Built(event) end)
 game.on_event({defines.events.on_entity_died,defines.events.on_robot_pre_mined_item,defines.events.on_preplayer_mined_item,},function(event) On_Remove(event) end)
@@ -50,7 +46,6 @@ function On_Load()
 		force.reset_technologies() 
 	end
 	if global.ArtifactCollectors ~= nil then
-        --print('Onload subscription')
 		game.on_event(defines.events.on_tick, function(event) ticker(event.tick) end)
         global.next_check= global.next_check or game.tick+interval
         global.next_collector= global.next_collector or 1
@@ -62,11 +57,8 @@ end
 
 ----
 function subscribe_ticker(tick)
-	--this function subsribes handler to on_tick event and also sets global values used by it
-	--it exists merelly for a convenience grouping
-    --print('mod initialized')
-    --print(tick)
-    --print(global.next_check)
+	--this function subscribes handler to on_tick event and also sets global values used by it
+	--it exists merely for a convenience grouping
 	game.on_event(defines.events.on_tick,function(event) ticker(event.tick) end)
 	global.ArtifactCollectors= {}
 	global.next_check=game.tick+interval
@@ -109,7 +101,7 @@ function On_Remove(event)
         --and here artifacts=nil would not cut it.
             global.ArtifactCollectors=nil--I'm not sure this wins much, on it's own
             game.on_event(defines.events.on_tick, nil);
-            --but it's  surelly better done here than during on_tick
+            --but it's surely better done here than during on_tick
         end
     end
 end
@@ -119,9 +111,6 @@ function ticker(tick)
 	--this function provides the smooth handling of all collectors within certain span of time
 	--it requires global.ArtifactCollectors, global.next_check, global.next_collector to do that
 	if game.tick==global.next_check then
-        --print('mod working')
-        --print(tick)
-        --print(global.next_collector)
 		local collectors=global.ArtifactCollectors
          writeDebug(#collectors)
 		for i=global.next_collector,#collectors,interval do
@@ -139,20 +128,15 @@ function ProcessCollector(collector)
      writeDebug("mod looking for items")
 	local items
 	local inventory
-	local Count_Check=global.ArtifactCollectors
-	--if Count_Check ~= nil then
-	if Count_Check > 0 then
-		items = collector.surface.find_entities_filtered({area = {{x = collector.position.x - radius, y = collector.position.y - radius}, {x = collector.position.x + radius, y = collector.position.y + radius}}, name = "item-on-ground"})
-		if #items > 0 then
-			inventory = collector.get_inventory(chestInventoryIndex)
-			for i=1,#items do
-				local stack = items[i].stack
-				--print(stack.name)
-				if filters[stack.name] == 1 and inventory.can_insert(stack) then
-					 inventory.insert(stack)
-					 items[i].destroy()
-					 break
-				end
+	items = collector.surface.find_entities_filtered({area = {{x = collector.position.x - radius, y = collector.position.y - radius}, {x = collector.position.x + radius, y = collector.position.y + radius}}, name = "item-on-ground"})
+	if #items > 0 then
+		inventory = collector.get_inventory(chestInventoryIndex)
+		for i=1,#items do
+			local stack = items[i].stack
+			if filters[stack.name] == 1 and inventory.can_insert(stack) then
+				 inventory.insert(stack)
+				 items[i].destroy()
+				 break
 			end
 		end
 	end
