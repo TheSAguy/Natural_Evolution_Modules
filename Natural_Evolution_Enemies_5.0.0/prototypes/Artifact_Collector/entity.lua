87d832164210102b9a182b76956853fba014161e
TheSAguy <vvwmisc@hotmail.com>
2015-10-10 21:50:55 -0700

    10.10
    Tweaks

--- Natural_Evolution_Enemies_5.0.0/prototypes/Artifact_Collector/entity.lua
+++ Natural_Evolution_Enemies_5.0.0/prototypes/Artifact_Collector/entity.lua
@@ -35,7 +35,7 @@ data:extend(
 			}
 		},
 		{
-			type = "logistic-container",
+			type = "container",
 			--type = "container",
 			name = "Artifact-collector-area",
 			icon = "__Natural_Evolution_Enemies__/graphics/icons/Artifact-chest-icon.png",
@@ -43,20 +43,12 @@ data:extend(
 			minable = {mining_time = 0.5, result = "Artifact-collector-area"},
 			max_health = 150,
 			corpse = "small-remnants",
-			resistances = 
-			{
-				{
-					type = "fire",
-					percent = 70
-				}
-			},
 			collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
 			selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
 			order = "i[items]-i[Artifact-collector]",
 			subgroup = "storage",
 			fast_replaceable_group = "container",
 			inventory_size = 48,
-			logistic_mode = "passive-provider",
 			open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
 			close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
 			vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
