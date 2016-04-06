304a3aa724b9f7d9c30b79ef67a6ecd828ad5300
TheSAguy <vvwmisc@hotmail.com>
2016-04-06 10:28:52 -0700

    Tweak

--- Natural_Evolution_Expansion/data-updates.lua
+++ Natural_Evolution_Expansion/data-updates.lua
@@ -21,15 +21,15 @@ if NEConfig.ScienceCost then
 		end
 	end
 
+end
+
 
 	--- Rocket Part Cost Tweaks	
-	if NEConfig.HarderEndGame then
-		for _, recipe in pairs({"low-density-structure", "rocket-fuel", "rocket-control-unit", "rocket-part", "satellite"}) do
-			for _, ingredient in pairs(data.raw.recipe[recipe].ingredients) do
-				ingredient[2] = ingredient[2] * 2
-			end
+if NEConfig.HarderEndGame then
+	for _, recipe in pairs({"low-density-structure", "rocket-fuel", "rocket-control-unit", "rocket-part", "satellite"}) do
+		for _, ingredient in pairs(data.raw.recipe[recipe].ingredients) do
+			ingredient[2] = ingredient[2] * 2
 		end
 	end
-
-
 end
+
