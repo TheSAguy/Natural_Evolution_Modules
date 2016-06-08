516bddece68453c48dd243cea74956c4165b39d5
TheSAguy <vvwmisc@hotmail.com>
2016-06-08 13:57:50 -0700

    order tweaks

--- Natural_Evolution_Enemies/prototypes/Artifact_Collector/item.lua
+++ Natural_Evolution_Enemies/prototypes/Artifact_Collector/item.lua
@@ -5,7 +5,7 @@ data:extend(
 			name = "Artifact-collector-area",
 			icon = "__Natural_Evolution_Enemies__/graphics/icons/Artifact-chest-icon.png",
 			flags = {"goes-to-quickbar"},
-			subgroup = "Tools",
+			subgroup = "Natural-Evolution",
 			order = "d[Artifact-collector]",
 			place_result = "Artifact-collector-area",
 			stack_size = 50
